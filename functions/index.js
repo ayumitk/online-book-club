e95e284ec6b1f1ad64640d558b37d761daf1ca03
ayumitk <ayumee528@gmail.com>
2020-01-03 07:46:43 -0800

    update

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello, ninjas!');
+});
+
+const createNotification = (notification) => (admin.firestore().collection('notification')
+  .add(notification)
+  .then((doc) => {
+    console.log('notification added', doc);
+  }));
+
+exports.roomCreated = functions.firestore
+  .document('rooms/{roomId}')
+  .onCreate((doc) => {
+    const room = doc.data();
+    const notification = {
+      content: 'Added a new room',
+      user: room.authorName,
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    };
+
+    return createNotification(notification);
+  });
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Notifications = () => (
-  <div>
-    <p>Notifications</p>
+const Notifications = (props) => (
+  <div className="section">
+    <div className="card z-depth-0">
+      <div className="card-content">
+        <span className="card-title">Notifications</span>
+        <ul>
+          <li>Notification</li>
+          <li>Notification</li>
+          <li>Notification</li>
+          <li>Notification</li>
+        </ul>
+      </div>
+    </div>
   </div>
 );
 
